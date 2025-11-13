b8e9da8ca7cb2e3f135f50f7f5cc297740c264c7
Destin <berniedestin7@gmail.com>
2025-11-13 10:26:00 -0500

    Fixed bad change to Create User Table migration

--- db/migrate/20251106145928_create_users.rb
+++ db/migrate/20251106145928_create_users.rb
@@ -4,11 +4,6 @@ class CreateUsers < ActiveRecord::Migration[8.1]
       t.string :email
       t.string :first_name
       t.string :last_name
-      t.string :accountability_email
-      t.boolean :is_married
-      t.boolean :has_children
-      t.date :birthday
-      t.integer :church_size
 
       t.timestamps
     end
